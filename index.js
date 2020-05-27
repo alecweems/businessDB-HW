44fbb57e96c825586a3ed0019f397d905b221568
alecweems <a.weems721@gmail.com>
2020-05-27 19:53:24 -0400

    pseudo-code

---
+++ index.js
@@ -0,0 +1,37 @@
+// * Add departments, roles, employees
+
+//   * View departments, roles, employees
+
+//   * Update employee roles
+
+
+//7 different options
+// ask the user , which of those 7 yould you like to do.
+// option 1 - view department   
+//    select name from departments
+//    dislay to user
+// option 2 - view roles
+//    select title, salary from roles
+//    display to user
+// option 3 view employees
+//    select first_name, last_name, from employees 
+//    display all employees
+// option 4 add department
+//     ask the user for a department name
+//     insert the new department in the departments table
+// option 5 add role
+//     select id, name from departments
+//     ask the user for which department (in order to get the id)
+//     ask the user for a tile, salary
+//     add the title, salary,  department id to the role table 
+// option 6 add employee
+//     select id, title from role
+//     ask user which role (to get the role id)
+//     add user for first_name, last_name
+//     add first_name, last_name, role_id to employee table
+// option 7 update employee role
+//     select first_name, last_name from employee
+//     ask user which employee to update
+//     select title from role table (provides a list of roles)
+//     ask user what is the new role 
+//     update the role of the employee selected
